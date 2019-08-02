7e8a6a429ca75066a19195455968b7bcabe0eb9c
Mateusz Fila <mf358630@okwf.fuw.edu.pl>
2019-08-02 19:10:59 +0200

    sending sigint destroy objects properly

--- client_gui/main.cpp
+++ client_gui/main.cpp
@@ -1,14 +1,21 @@
 #include "include/mainwindow.h"
 #include <QApplication>
 #include <QStyleFactory>
+#include <signal.h>
+void stopHandler(int sig) {
+        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "received ctrl-c");
+    QApplication::exit();
+    }
 int main(int argc, char *argv[])
-{
+{    signal(SIGINT, stopHandler);
+     signal(SIGTERM, stopHandler);
     QApplication a(argc, argv);
     a.setStyle(QStyleFactory::create("Fusion"));
     QCoreApplication::setOrganizationName("FUW");
     QCoreApplication::setApplicationName("ELITPCdcs client");
     MainWindow w;
     w.show();
-
+  //   signal(SIGINT,SIG_DFL);
+  //   signal(SIGTERM,SIG_DFL);
     return a.exec();
 }
