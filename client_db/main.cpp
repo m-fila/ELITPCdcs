1687f59441c71fe8ed50234dc59c7697611ce2b3
MFila <mfila@fuw.edu.pl>
2019-11-12 12:31:50 +0100

    added logginh tpg362

--- client_db/main.cpp
+++ client_db/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "include/hmp_variable.h"
 #include "include/dt1415_variable.h"
+#include "include/tpg362_variable.h"
 #include "../common/loader.h"
 int main(int argc, char *argv[])
 {
@@ -17,14 +18,19 @@ int main(int argc, char *argv[])
     opc_client client(loader.getAddress(),loader.getPort());
     
     client.Database.open("test.db");
-
     for (auto L : loader.getItems()) {
 
         if(L.device=="HMP2020"){
            client.addVariable(new HMP2020Variable(L.Id,"Measurements"));
         }
+        else if(L.device=="HMP4040"){
+           client.addVariable(new HMP4040Variable(L.Id,"Measurements"));
+        }
         else if(L.device=="DT1415ET"){
             client.addVariable(new dt1415_variable(L.Id,"Measurements"));
+        }
+        else if(L.device=="TPG362"){
+            client.addVariable(new TPG362Variable(L.Id,"Measurements"));
         }
 
         else{
--- client_db/src/hmp_variable.cpp
+++ client_db/src/hmp_variable.cpp
@@ -8,7 +8,7 @@ HMPVariable::HMPVariable(std::string OName,std::string VName, int size): templat
 
 std::string HMPVariable::translateKeys(){
 //    std::string str="UA_timestamp INTEGER PRIMARY KEY"
-            ", V1 REAL, V2 REAL, C1 REAL, C2 REAL";
+//            ", V1 REAL, V2 REAL, C1 REAL, C2 REAL";
 //    return str;
 
        std::ostringstream oss;
