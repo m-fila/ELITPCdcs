661fb784fff07f74f07bb52a0f4b0b7e5d997761
Mateusz Fila <mfila@fuw.edu.pl>
2020-02-06 18:25:00 +0100

    added addMethod higher level function

--- COREdcs/server/include/opc_object.h
+++ COREdcs/server/include/opc_object.h
@@ -3,6 +3,15 @@
 #include <open62541/server.h>
 #include <open62541/plugin/log_stdout.h>
 #include <string>
+#include <vector>
+//dataType, name ,description
+//example: UA_TYPES[UA_TYPES_BOOLEAN], "Status", "ON/OFF" 
+struct methodInputOutput{
+    UA_DataType dataType;
+    std::string name;
+    std::string description;
+};
+
 class OpcObject
 {
 public:
@@ -13,6 +22,10 @@ protected:
     virtual void addVariable3(UA_Server *server,UA_NodeId *VariableNodeId,std::string VariableName,UA_DataType VariableType);
     virtual UA_NodeId addVariable2(UA_Server *server,std::string VariableName,UA_DataType VariableType,UA_NodeId TypeNodeId); //auto-asign nodeid and return it
     virtual void addVariable(UA_Server *server,std::string VariableName,UA_DataType VariableType,UA_NodeId TypeNodeId); //nodeid_string(1,VariableName)
+    //scalar input/output only
+    virtual void addMethod(UA_Server *server, std::string methodName, UA_MethodCallback methodCallback,
+                            std::string methodDescription,std::vector<methodInputOutput> inputs,
+                            std::vector<methodInputOutput> outputs,UA_NodeId *methodNodeId=nullptr);
     const std::string objectName;
     UA_NodeId objectNodeId;
 };
--- COREdcs/server/src/opc_object.cpp
+++ COREdcs/server/src/opc_object.cpp
@@ -72,3 +72,53 @@ void OpcObject::addVariable3(UA_Server *server,UA_NodeId *VariableNodeId,std::st
     UA_VariableAttributes_deleteMembers(&vattr);
     UA_QualifiedName_deleteMembers(&VariableQName);
 }
+
+void OpcObject::addMethod(UA_Server *server, std::string methodName, UA_MethodCallback methodCallback,
+                          std::string methodDescription,std::vector<methodInputOutput> inputs,
+                          std::vector<methodInputOutput> outputs,UA_NodeId *methodNodeId){
+    UA_Argument *inputArguments=static_cast<UA_Argument*>(UA_Array_new(inputs.size(),&UA_TYPES[UA_TYPES_ARGUMENT]));
+    for (int i=0; i<inputs.size();i++){
+        UA_Argument_init(&inputArguments[i]);
+        inputArguments[i].description = UA_LOCALIZEDTEXT_ALLOC("en-US", inputs.at(i).description.c_str());
+        inputArguments[i].name = UA_String_fromChars(inputs.at(i).name.c_str());
+        inputArguments[i].dataType = inputs.at(i).dataType.typeId;
+        inputArguments[i].valueRank = UA_VALUERANK_SCALAR;
+    }
+    UA_Argument *outputArguments=static_cast<UA_Argument*>(UA_Array_new(outputs.size(),&UA_TYPES[UA_TYPES_ARGUMENT]));
+    for (int i=0; i<outputs.size();++i){
+        UA_Argument_init(&outputArguments[i]);
+        outputArguments[i].description = UA_LOCALIZEDTEXT_ALLOC("en-US", outputs.at(i).description.c_str());
+        outputArguments[i].name = UA_String_fromChars(outputs.at(i).name.c_str());
+        outputArguments[i].dataType = outputs.at(i).dataType.typeId;
+        outputArguments[i].valueRank = UA_VALUERANK_SCALAR;
+    }
+    UA_MethodAttributes methodAttr = UA_MethodAttributes_default;
+    methodAttr.description = UA_LOCALIZEDTEXT_ALLOC("en-US",methodDescription.c_str());
+    methodAttr.displayName = UA_LOCALIZEDTEXT_ALLOC("en-US",methodName.c_str());
+    methodAttr.executable = true;
+    methodAttr.userExecutable = true;
+    UA_QualifiedName methodQName= UA_QUALIFIEDNAME_ALLOC(1, methodName.c_str());
+    UA_Server_addMethodNode(server, UA_NODEID_NULL,
+                            objectNodeId,
+                            UA_NODEID_NUMERIC(0, UA_NS0ID_HASORDEREDCOMPONENT),
+                            methodQName,
+                            methodAttr, methodCallback,
+                            inputs.size(),inputArguments, outputs.size(), outputArguments,this, methodNodeId);
+    UA_MethodAttributes_deleteMembers(&methodAttr);
+    UA_QualifiedName_deleteMembers(&methodQName);
+
+    
+    //for(int i=0;i<inputs.size();++i){
+    //    UA_Argument_deleteMembers(&inputArguments[i]);
+    //}
+    //UA_Argument_delete(inputArguments);
+    //for(int i=0;i<outputs.size();++i){
+    //    UA_Argument_deleteMembers(&outputArguments[i]);
+    //}
+    //UA_Argument_delete(outputArguments);
+    
+    UA_Array_delete(inputArguments,inputs.size(),&UA_TYPES[UA_TYPES_ARGUMENT]);
+    UA_Array_delete(outputArguments,outputs.size(),&UA_TYPES[UA_TYPES_ARGUMENT]);
+}
+
+    
