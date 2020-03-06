52fe0b54ab83549918ae7d2e60e4d41e07b99e44
mfila <mfila@fuw.edu.pl>
2020-03-07 00:41:33 +0100

    changed data format in .db filenames

--- ELITPCdcs/clientDB/bin/main.cpp
+++ ELITPCdcs/clientDB/bin/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
     auto now = std::chrono::system_clock::now();
     auto in_time_t = std::chrono::system_clock::to_time_t(now);
     std::stringstream ss;
-    ss <<"./dcs"<<std::put_time(std::localtime(&in_time_t), "%Y-%m-%d-%X")<<".db";
+    ss <<"./dcs"<<std::put_time(std::localtime(&in_time_t), "%Y-%m-%d-%H-%M-%S")<<".db";
     databasePath = ss.str();
     std::cout << "Missing databasePath configuration. Using " << databasePath<< std::endl;
   }
