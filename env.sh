bde15176c6d5857232007af4675ba885df3d4526
Mateusz Fila <mfila@fuw.edu.pl>
2020-03-06 17:38:30 +0100

    added install target

--- env.sh
+++ env.sh
@@ -1,2 +1,4 @@
-#export CMAKE_PREFIX_PATH=CMAKE_PREFIX_PATH:/opt/soft/open62541
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/home/mfila/open62541-install
+#open62541_path=/home/mfila/open62541-install
+open62541_path=/opt/soft/open62541
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:${open62541_path}
+export open62541_LIB=${open62541_path}/lib
