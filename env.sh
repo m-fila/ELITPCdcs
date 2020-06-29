424c6e65ec4d5297b38c1733d717bd83de6fb4cd
Mateusz Fila <mf358630@okwf.fuw.edu.pl>
2020-06-29 20:31:22 +0200

    using open62541 v1.1

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 #open62541_path=/home/mfila/open62541-install
-open62541_path=/opt/soft/open62541-1.1rc
+open62541_path=/opt/soft/open62541-v1.1
 export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:${open62541_path}
 export open62541_LIB=${open62541_path}/lib
