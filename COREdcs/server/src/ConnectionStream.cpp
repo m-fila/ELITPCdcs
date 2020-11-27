75db294b54e3335d6f699e8011ad6fc37be1e3b6
mfila <mfila@fuw.edu.pl>
2020-11-27 03:27:04 +0100

    added striping responses up to \r\n instead of \n

--- COREdcs/server/src/ConnectionStream.cpp
+++ COREdcs/server/src/ConnectionStream.cpp
@@ -1,5 +1,9 @@
 #include "ConnectionStream.h"
 #include <stdexcept>
+
+#ifdef DEBUG_COMMUNICATION
+#include <iostream>
+#endif
 ConnectionStream::ConnectionStream() {}
 
 ConnectionStream::~ConnectionStream() { close(m_sd); }
@@ -12,6 +16,9 @@ ssize_t ConnectionStream::send(char *buffer, size_t len) {
 }
 
 ssize_t ConnectionStream::send(std::string s) {
+#ifdef DEBUG_COMMUNICATION
+    std::cout << "S: " << s << std::endl;
+#endif
     return send((char *)s.c_str(), s.length());
 }
 
@@ -31,9 +38,17 @@ std::string ConnectionStream::receive() {
         response.append(buffer, len);
         //  std::fill(&buffer[0], &buffer[0]+len, 0);
     } while(len == 1024);
+    // remove new line from answer
     if(!response.empty() && response[response.length() - 1] == '\n') {
         response.erase(response.length() - 1);
-    }  // remove new line from answer
+    }
+    // remove new carriage return from answer
+    if(!response.empty() && response[response.length() - 1] == '\r') {
+        response.erase(response.length() - 1);
+    }
+#ifdef DEBUG_COMMUNICATION
+    std::cout << "R: " << response << std::endl;
+#endif
     return response;
 }
 
