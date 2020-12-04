db6de1bb2ace485bd8503272e430eacb2bde3859
mfila <mfila@fuw.edu.pl>
2020-12-04 12:24:02 +0100

    fixed missing startup logs

--- COREdcs/server/src/DCSServer.cpp
+++ COREdcs/server/src/DCSServer.cpp
@@ -9,8 +9,8 @@ DCSServer::DCSServer(std::string address, int port) {
     }
     server = UA_Server_new();
     config = UA_Server_getConfig(server);
-    UA_ServerConfig_setMinimal(config, port, nullptr);
     config->logger = *DCSLogger::getLogger();
+    UA_ServerConfig_setMinimal(config, port, nullptr);
     UA_String hostname = UA_STRING_ALLOC(address.c_str());
     UA_ServerConfig_setCustomHostname(config, hostname);
     UA_String_deleteMembers(&hostname);
