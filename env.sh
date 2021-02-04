5e0170486ef94f12f114c909be776b26e0b3b09c
mfila <mfila@fuw.edu.pl>
2021-02-04 01:10:30 +0100

    updated path to default open62541 installation

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-#open62541_path=/home/mfila/open62541-install
-open62541_path=/opt/soft/open62541-v1.1
+open62541_path=/opt/soft/open62541-v1.1-install/
 export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:${open62541_path}
 export open62541_LIB=${open62541_path}/lib
+
