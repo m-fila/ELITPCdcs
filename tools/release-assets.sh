bf75b592a576446cde51e55c0ab1578e6bc72d34
Mateusz Fila <mfila@fuw.edu.pl>
2021-02-25 19:22:43 +0100

    fixing tokens

--- tools/release-assets.sh
+++ tools/release-assets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -k --request POST \
-    --header "PRIVATE-TOKEN: $CI_JOB_TOKEN" \
+    --header "JOB-TOKEN: $CI_JOB_TOKEN" \
     --data name="ELITPC_DCS-$CI_COMMIT_TAG-Linux.deb" \
     --data url="$CI_API_V4_URL/projects/$CI_PROJECT_ID/jobs/artifacts/$$CI_COMMIT_TAG/build/ELITPC_DCS-$CI_COMMIT_TAG-Linux.deb?job=package" \
     "$CI_API_V4_URL/projects/$CI_PROJECT_ID/releases/$CI_COMMIT_TAG/assets/links"
