da82285caa5406a85774bfcf8287231968277473
Mateusz Fila <mfila@fuw.edu.pl>
2021-02-25 20:14:17 +0100

    fixes

--- tools/release-assets.sh
+++ tools/release-assets.sh
@@ -2,5 +2,5 @@
 
 curl -k --header "JOB-TOKEN: $CI_JOB_TOKEN" \
     --data name="ELITPC_DCS-$CI_COMMIT_TAG-Linux.deb" \
-    --data url="$CI_API_V4_URL/projects/$CI_PROJECT_ID/jobs/artifacts/$$CI_COMMIT_TAG/build/ELITPC_DCS-$CI_COMMIT_TAG-Linux.deb?job=package" \
+    --data url="$CI_API_V4_URL/projects/$CI_PROJECT_ID/jobs/artifacts/$CI_COMMIT_TAG/build/ELITPC_DCS-$CI_COMMIT_TAG-Linux.deb?job=package" \
     --request POST "$CI_API_V4_URL/projects/$CI_PROJECT_ID/releases/$CI_COMMIT_TAG/assets/links"
